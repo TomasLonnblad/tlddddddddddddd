25ff5261a8f0778d8ba99492ea2991513d1461b7
TomasLonnblad <tomaslonnblad@gmail.com>
2025-09-04 09:43:39 +0200

    x

---
+++ app/app.vue
@@ -0,0 +1,22 @@
+<script setup>
+const message = ref('Nuxt')
+
+function hello() {
+  sayHello(message.value)
+}
+</script>
+
+<template>
+  <NuxtExample dir="features/auto-imports">
+    <h1>Demo with auto imports</h1>
+    <form
+      class="flex gap-2"
+      @submit.prevent="hello"
+    >
+      <CustomInput v-model="message" />
+      <UButton type="submit">
+        Hello
+      </UButton>
+    </form>
+  </NuxtExample>
+</template>
